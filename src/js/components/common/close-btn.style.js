b9b818107b16876693198b35925f4f9a71adf49f
alfa <alfa@invisibleinstitute.com>
2016-05-09 14:15:46 +0700

    Fix close button style for ipad

--- src/js/components/common/close-btn.style.js
+++ src/js/components/common/close-btn.style.js
@@ -22,6 +22,7 @@ export const closeButtonIconStyle = {
   pseudo: {
     height: '1px',
     transform: 'rotate(45deg)',
+    transformOrigin: 'center',
     position: 'absolute',
     width: '100%',
     top: '50%',
