3eff0c9a468241442d422b82ec73de0f11374ced
golf+india <golf+india@invisibleinstitute.com>
2016-12-30 10:52:56 +0700

    [c] Revert 871b - Setup smooch.io on v2

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,6 @@ import Perf from 'react-addons-perf';
 import 'babel-polyfill';
 import 'polyfill';
 import RouterRoot from 'components/router-root';
-import Smooch from 'smooch';
-
-Smooch.init({ appToken: '5k7ui26dsbbtcryswdc72nrzt' });
 
 global.Perf = Perf;
 TwitterWidgetsLoader.load(() => {});
